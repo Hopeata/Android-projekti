c69e2b5836e3426c577f098b525fcde04b26de75
Hopeata <valeria.verkhovskaia@gmail.com>
2012-11-13 17:13:35 +0200

    Note-luokka plus säätöä

---
+++ VirtualPostIt/src/android/virtualpostit/Note.java
@@ -0,0 +1,31 @@
+package android.virtualpostit;
+
+import java.util.Date;
+
+public class Note {
+	
+	String note = "";
+	Date timestamp;
+	int id = 0;
+	
+	public Note(int id, String note, Date timestamp) {
+		this.note = note;
+		this.timestamp = timestamp;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getNote() {
+		return note;
+	}
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+
+
+
+}
--- VirtualPostIt/src/android/virtualpostit/StorageManager.java
+++ VirtualPostIt/src/android/virtualpostit/StorageManager.java
@@ -5,27 +5,28 @@ import java.util.Date;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class PostItOpenHelper extends SQLiteOpenHelper {
+public class StorageManager extends SQLiteOpenHelper {
 
 	private static final String SQL_DATE_FORMAT = "YYYY-MM-DD HH:MM:SS.SSS";
 	private static final SimpleDateFormat SQL_DATE_FORMATTER = new SimpleDateFormat(SQL_DATE_FORMAT);
-    protected static final int DATABASE_VERSION = 3;
-    protected static final String POSTIT_DB_NAME = "PostIt";
-    protected static final String NOTE_TABLE = "Note"; 
-    protected static final String NOTE_TABLE_ID = "Id";
-    protected static final String NOTE_TABLE_NOTE = "Note";
-    protected static final String NOTE_TABLE_TS = "Timestamp";
-    protected final String CREATE_NOTE_TABLE =
+    private static final int DATABASE_VERSION = 3;
+    private static final String POSTIT_DB_NAME = "PostIt";
+    private static final String NOTE_TABLE = "Note"; 
+    private static final String NOTE_TABLE_ID = "Id";
+    private static final String NOTE_TABLE_NOTE = "Note";
+    private static final String NOTE_TABLE_TS = "Timestamp";
+    private final String CREATE_NOTE_TABLE =
             "create table " + NOTE_TABLE + " (" + NOTE_TABLE_ID + 
             " integer primary key autoincrement, " + NOTE_TABLE_NOTE +
             " text not null, " + NOTE_TABLE_TS + " text not null);";
 	SQLiteDatabase db = this.getWritableDatabase();
 	ContentValues cv = new ContentValues();
 
-    PostItOpenHelper(Context context) {
+    StorageManager(Context context) {
         super(context, POSTIT_DB_NAME, null, DATABASE_VERSION);
     }
     
@@ -42,21 +43,32 @@ public class PostItOpenHelper extends SQLiteOpenHelper {
 		
 	}
 	
-	public void insertNote(String name, Date timestamp) {
-		cv.put(NOTE_TABLE_NOTE, name);
+	public void insertNote(String note, Date timestamp) {
+		cv.put(NOTE_TABLE_NOTE, note);
 		cv.put(NOTE_TABLE_TS, SQL_DATE_FORMATTER.format(timestamp));
 		db.insert(NOTE_TABLE, null, cv);
-
 		db.close();
 	}
 	
 	public int UpdateNote(Note note)
 	  {
-	   cv.put(NOTE_TABLE_NOTE, note.getName());
-	   cv.put(NOTE_TABLE_TS, note.getAge());
-	   return db.update(NOTE_TABLE, cv, colID+"=?", 
-	    new String []{String.valueOf(note.getID())});   
+	   cv.put(NOTE_TABLE_NOTE, note.getNote());
+	   cv.put(NOTE_TABLE_TS, SQL_DATE_FORMATTER.format(note.getTimestamp()));
+	   return db.update(NOTE_TABLE, cv, NOTE_TABLE_ID + " = ?", 
+	    new String []{String.valueOf(note.getId())});   
+	  }
+	
+	public void DeleteNote(Note note)
+	  {
+	   SQLiteDatabase db=this.getWritableDatabase();
+	   db.delete(NOTE_TABLE, NOTE_TABLE_ID + " = ?", new String [] {String.valueOf(note.getId())});
+	   db.close();
 	  }
 	
 	
+	Cursor getAllNotes()
+	  {
+	   Cursor cur=db.rawQuery("SELECT " + NOTE_TABLE_NOTE +" from "+ NOTE_TABLE,new String [] {});
+	   return cur;
+	  }
 }
