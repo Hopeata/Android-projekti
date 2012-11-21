f94a6de89e35882c04cd072a284429eb679d5738
Hopeata <valeria.verkhovskaia@gmail.com>
2012-11-21 23:50:53 +0200

    Siivousta

--- VirtualPostIt/src/android/virtualpostit/PostIt.java
+++ VirtualPostIt/src/android/virtualpostit/PostIt.java
@@ -20,7 +20,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PostIt extends ListActivity {
+public class PostIt extends Activity {
 
 	  private ListView mainListView ;  
 	  private NoteArrayAdapter listAdapter ; 
